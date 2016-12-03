e4ef590d7b45ee1631b5cfdc760bf1f272a1494d
Nikunj Jain <nikunj.jain@berkeley.edu>
2016-12-02 21:49:21 -0800

    Solved Problem 54

--- problem_54.py
+++ problem_54.py
@@ -7,9 +7,9 @@ class Card:
     def __repr__(self):
         return self.number+self.suit
 
-    def __init__(self, suit, number):
-        self.suit = suit
-        self.number = number
+    def __init__(self, value):
+        self.suit = value[1]
+        self.number = value[0]
 
 def has_straight(hand):
     cards = sorted(hand.cards, key=lambda card: all_cards[card.number])
@@ -77,7 +77,7 @@ def has_royal_flush(hand):
     return False
 
 def highest_card(hand):
-    return max([card.number for card in hand.cards], key=lambda x:all_cards[x])
+    return True, (max(hand.cards, key=lambda x:all_cards[x.number]),)
 
 functions = (highest_card, has_pair, has_two_pair, has_three_of_a_kind, has_straight, has_flush, has_full_house, has_four_of_a_kind, has_straight_flush, has_royal_flush)
 
@@ -104,3 +104,21 @@ class Hand:
         
 
 def main(filename = "problem_54_data"):
+    f = open(filename)
+    lines = list(f)
+    games = [[Hand(cards[:5]), Hand(cards[5:10])] for cards in [[Card(word[:2]) for word in line.split(" ")] for line in lines]]
+    games_copy = games[:]
+    count1, ecount = (0,0)
+    for game in games_copy:
+        hand1, hand2 = game
+        value1, value2 = hand1.value(), hand2.value()
+        if value1[0] > value2[0]: count1 +=1 
+        elif value1[0] == value2[0]: 
+            win1, win2 = value1[1][1][0], value2[1][1][0]
+            if win1.number == win2.number:
+                print(hand1, hand2)
+                ecount +=1
+            elif win1.number > win2.number:
+                count1+=1
+
+    print(count1, ecount)
